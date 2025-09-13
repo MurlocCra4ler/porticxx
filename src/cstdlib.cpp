a2bc7d8dc366425cfacb659eeffa6e92ee75a3f9
Johannes Czekay <johannes.czekay@gmail.com>
2025-09-13 21:23:01 +0200

    add exit

--- src/cstdlib.cpp
+++ src/cstdlib.cpp
@@ -5,4 +5,6 @@ namespace std {
 
 [[noreturn]] void abort() noexcept { arch::current_arch::abort(); }
 
+[[noreturn]] void exit(int exit_code) { arch::current_arch::exit(exit_code); }
+
 } // namespace std
